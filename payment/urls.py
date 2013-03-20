1e27303213a5f7d5a7c0ac5624ff7186792fd1b4
Rob Fox <thefoxbrain@gmail.com>
2013-03-20 16:13:48 +0000

    linking to members payments

--- payment/urls.py
+++ payment/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('payment.views',
     (r'^$', 'pay_index'),
     # ex: /members/5/
     (r'^(?P<payment_id>\d+)/$', 'pay_detail'),    
+    # ex: /members/5/
+    (r'^member/(?P<last_name>\w+)/$', 'pay_member_index'),
 )
--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,render_to_response
 
 from members.models import Payment
 
@@ -6,6 +6,16 @@ def pay_index(request):
     latest_payment_list = Payment.objects.all().order_by('-payment_date')[:5]
     context = {'latest_payment_list': latest_payment_list}
     return render(request, 'payment/base_pay_index.html', context)
+"""
+def pay_member_index(request):
+    member_payment_list = Payment.objects.filter(member__id = 1)
+    context = {'member_payment_list': member_payment_list}
+    return render(request, 'payment/base_pay_member_index.html', context)
+"""    
+
+def pay_member_index(request, last_name):
+    member_payment_list = Payment.objects.filter(member__last_name__exact = last_name)
+    return render_to_response('payment/base_pay_member_index.html', {'member_payment_list': member_payment_list })
 
 def pay_detail(request, payment_id):
     payment = get_object_or_404(Payment, pk=payment_id)
