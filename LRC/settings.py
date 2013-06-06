6d7b75075286f4bbff974940fa30365d6b34745f
Rob Fox <thefoxbrain@gmail.com>
2013-06-06 22:11:26 +0100

    date picker

--- LRC/settings.py
+++ LRC/settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'pagination.middleware.PaginationMiddleware',
-    'django_sorting.middleware.SortingMiddleware',
+    # 'django_sorting.middleware.SortingMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
